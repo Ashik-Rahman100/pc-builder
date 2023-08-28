74bde9294819b038b235185d39c09955435f48cc
Ashik-Rahman100 <ashik5515@gmail.com>
2023-08-29 01:10:53 +0600

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+
+const NotFoundPage = () => {
+  return (
+    <div  className="  ">
+      <h1 className="flex justify-center items-center h-[100vh] text-6xl text-red">
+        <span className="text-center">Opps!</span> 404 Page Not Found !!! 
+      </h1>
+      
+    </div>
+  );
+};
+
+export default NotFoundPage;
